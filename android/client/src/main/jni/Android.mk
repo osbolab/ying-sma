152f3ce4beee2affe3cd9a6918dafb8b58783c2f
Matthew Barnard <msbarnar@gmail.com>
2015-02-16 15:36:32 -0700

    add SMA headers to global includes, include android logging

--- android/client/src/main/jni/Android.mk
+++ android/client/src/main/jni/Android.mk
@@ -4,5 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := smajni
 LOCAL_SRC_FILES := jninodecontainer.cpp
+LOCAL_CFLAGS := -I../../../../include/ -I../../../../../include/
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
