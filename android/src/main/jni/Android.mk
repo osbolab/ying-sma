5ba897e688f0d644d828ffb9a01798b0ef3aab66
Matthew Barnard <msbarnar@gmail.com>
2015-02-17 01:50:18 -0700

    recursive *.cpp detection

--- android/src/main/jni/Android.mk
+++ android/src/main/jni/Android.mk
@@ -1,12 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+SMA_ROOT := $(abspath ../../../..)
+SMA_SRC := $(SMA_ROOT)/src
+SMA_INC := $(SMA_ROOT)/include
 
-LOCAL_MODULE    := smajni
-SMA_IMPL_SRCS := $(wildcard $(LOCAL_PATH)/../native/*.cpp)
-JNI_IFACE_SRCS := $(wildcard $(LOCAL_PATH)/*.cpp)
-LOCAL_SRC_FILES := $(SMA_IMPL_SRCS:$(LOCAL_PATH)/%=%) $(JNI_IFACE_SRCS:$(LOCAL_PATH)/%=%)
-LOCAL_CFLAGS := -I../../../include/ -I../../../../include/
+include $(CLEAR_VARS)
+LOCAL_MODULE := smanative
+
+SMA_SOURCES := $(wildcard $(SMA_SRC)/*.cpp)
+SMA_SOURCES += $(wildcard $(SMA_SRC)/**/*.cpp)
+SMA_SOURCES += $(wildcard $(SMA_SRC)/**/**/*.cpp)
+
+JNI_SOURCES := $(wildcard $(LOCAL_PATH)/*.cpp)
+
+LOCAL_SRC_FILES := $(SMA_SOURCES:$(LOCAL_PATH)/%=%) $(JNI_SOURCES:$(LOCAL_PATH)/%=%)
+LOCAL_CFLAGS := -I../../../include/ -I$(SMA_INC)
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
