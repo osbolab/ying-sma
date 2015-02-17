07e57f0345286159cf2cc3cfb4efe55933c8abec
Matthew Barnard <msbarnar@gmail.com>
2015-02-17 01:50:44 -0700

    correct bad package name

--- android/src/main/java/edu/asu/sma/NavActivity.java
+++ android/src/main/java/edu/asu/sma/NavActivity.java
@@ -1,19 +1,16 @@
 package edu.asu.sma;
 
-import android.app.Activity;
-
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-
-import edu.asu.sma.client.R;
 
 
 public class NavActivity extends Activity
--- android/src/main/java/edu/asu/sma/NavigationDrawerFragment.java
+++ android/src/main/java/edu/asu/sma/NavigationDrawerFragment.java
@@ -1,16 +1,16 @@
 package edu.asu.sma;
 
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,11 +22,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import edu.asu.sma.client.R;
-
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
- * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
+ * See the <a href="https://developer.android.com/design/patterns/navigation-drawer
+ * .html#Interaction">
  * design guidelines</a> for a complete explanation of the behaviors implemented here.
  */
 public class NavigationDrawerFragment extends Fragment {
