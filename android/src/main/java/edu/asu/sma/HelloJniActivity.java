2db03b044ede97538a8faeee8cbd713a3f1a80f5
Matthew Barnard <msbarnar@gmail.com>
2015-02-17 01:50:25 -0700

    remove test code

--- android/src/main/java/edu/asu/sma/HelloJniActivity.java
+++ android/src/main/java/edu/asu/sma/HelloJniActivity.java
@@ -2,8 +2,6 @@ package edu.asu.sma;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-
 
 public class HelloJniActivity extends Activity {
   /**
@@ -12,14 +10,6 @@ public class HelloJniActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-        /* Create a TextView and set its content.
-         * the text is retrieved by calling a native
-         * function.
-         */
-    TextView tv = new TextView(this);
-    tv.setText(new NodeContainer().stringFromJNI());
-    setContentView(tv);
 
     container = new NodeContainer();
     container.create();
