427fa2c13e2fab3ecf9ecd1aa47a2d842f1ac2ba
Matthew Barnard <msbarnar@gmail.com>
2015-02-16 22:47:19 -0700

    Compile all source files in native src dirs

--- android/src/main/jni/Android.mk
+++ android/src/main/jni/Android.mk
@@ -3,7 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := smajni
-LOCAL_SRC_FILES := jninodecontainer.cpp
+SMA_IMPL_SRCS := $(wildcard $(LOCAL_PATH)/../native/*.cpp)
+JNI_IFACE_SRCS := $(wildcard $(LOCAL_PATH)/*.cpp)
+LOCAL_SRC_FILES := $(SMA_IMPL_SRCS:$(LOCAL_PATH)/%=%) $(JNI_IFACE_SRCS:$(LOCAL_PATH)/%=%)
 LOCAL_CFLAGS := -I../../../include/ -I../../../../include/
 LOCAL_LDLIBS := -llog
 
