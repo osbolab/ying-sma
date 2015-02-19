35cf9b49978bebe95cc08435e65424e058019f46
Matthew Barnard <msbarnar@gmail.com>
2015-02-18 18:05:54 -0700

    network fragment displays neighbor list by periodically calling a native method on the background thread

---
+++ android/client/src/main/java/edu/asu/sma/client/NetworkFragment.java
@@ -0,0 +1,77 @@
+package edu.asu.sma.client;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import edu.asu.sma.NeighborHelper;
+
+public class NetworkFragment extends BaseFragment implements NativeConsumer {
+  private List<String> neighbors = new ArrayList<>();
+  private ArrayAdapter<String> neighbors_adapter;
+  private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+  public NetworkFragment() {
+    bind(this);
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    neighbors_adapter = new ArrayAdapter<>(activity,
+                                           android.R.layout.simple_list_item_1,
+                                           neighbors);
+    ListView neighbors_list = (ListView) activity.findViewById(R.id.neighbors_list);
+    neighbors_list.setAdapter(neighbors_adapter);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_network, container, false);
+  }
+
+  @Override
+  public void onNativeHandler(Handler handler) {
+    update();
+  }
+
+  private void update() {
+    if (nativeHandler == null)
+      return;
+
+    nativeHandler.post(new Runnable() {
+      @Override
+      public void run() {
+        final List<String> updated_neighbors = NeighborHelper.all();
+        activity.runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            neighbors.clear();
+            neighbors.addAll(updated_neighbors);
+            neighbors_adapter.notifyDataSetChanged();
+          }
+        });
+      }
+    });
+
+    executor.schedule(new Runnable() {
+      @Override
+      public void run() {
+        update();
+      }
+    }, 500, TimeUnit.MILLISECONDS);
+  }
+}
